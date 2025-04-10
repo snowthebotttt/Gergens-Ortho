c238479c491b8f7a12608e21a27d887b650b4ff3
Jared Gergen <gergen.jared1@gmail.com>
2025-04-09 22:16:07 -0700

    Fix: import Resolver type to match cast

--- src/components/forms/PrescriptionForm.tsx
+++ src/components/forms/PrescriptionForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, Resolver } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { motion } from "framer-motion";
